86b8f328576e9a52d76ae0f015dfe53d13c1b163
sascha.heilmeier <sheilmeier@gmail.com>
2023-04-02 22:40:15 +0200

    add unit tests for: CloseContext OpenContext TextContext

---
+++ tests/Unit/NodeProcessor/Context/CloseContextTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Netlogix\XmlProcessor\Tests\Unit\NodeProcessor\Context;
+
+use Netlogix\XmlProcessor\NodeProcessor\Context\CloseContext;
+use Netlogix\XmlProcessor\XmlProcessorContext;
+use PHPUnit\Framework\TestCase;
+
+class CloseContextTest extends TestCase
+{
+    private function getCloseContext(
+        ?XmlProcessorContext $context = NULL,
+        array $nodePath = ['foo', 'bar']
+    ): CloseContext
+    {
+        return new CloseContext(
+            $context ?? $this->getMockBuilder(XmlProcessorContext::class)
+            ->disableOriginalConstructor()
+            ->getMock(),
+            $nodePath
+        );
+    }
+
+    public function test__construct(): void
+    {
+        $nodeProcessorContext = $this->getCloseContext();
+        self::assertInstanceOf(CloseContext::class, $nodeProcessorContext);
+    }
+
+}
